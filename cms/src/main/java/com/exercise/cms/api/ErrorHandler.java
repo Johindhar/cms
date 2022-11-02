616af39e3ada189329bfeefa492baa368d1b04b8
Johindhar <johinaveen10@gmail.com>
2022-11-02 22:45:12 +0530

    KEM-27 Filter

--- cms/src/main/java/com/exercise/cms/api/ErrorHandler.java
+++ cms/src/main/java/com/exercise/cms/api/ErrorHandler.java
@@ -1,7 +1,14 @@
 package com.exercise.cms.api;
 
+import com.exercise.cms.exception.ApplicationError;
+import com.exercise.cms.exception.CustomerNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
@@ -9,5 +16,20 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestController
 public class ErrorHandler extends ResponseEntityExceptionHandler {
 
+    @Value("${api_doc_url}")
+    private String details;
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<ApplicationError> handleCustomerNotFoundException(CustomerNotFoundException exception, WebRequest webRequest){
+
+        ApplicationError error = new ApplicationError();
+        error.setCode(101);
+        error.setMessage(exception.getMessage());
+        error.setDetails(details);
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
+
+    }
 
 }
---
+++ cms/src/main/java/com/exercise/cms/filter/MyNewFilter.java
@@ -0,0 +1,17 @@
+package com.exercise.cms.filter;
+
+import jakarta.servlet.*;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class MyNewFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("The New Filter is called...");
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+}
